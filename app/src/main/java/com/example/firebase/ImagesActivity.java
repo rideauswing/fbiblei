30e342a8453edc0e7031914b64737f7741eb4722
rideauswing <dmikuivashev@gmail.com>
2020-05-19 19:11:55 +0500

    biblei_prototype v0.8

---
+++ app/src/main/java/com/example/firebase/ImagesActivity.java
@@ -0,0 +1,90 @@
+package com.example.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImagesActivity extends AppCompatActivity  implements ImageAdapter.OnItemClickListener {
+    private RecyclerView mRecyclerView;
+    private ImageAdapter mAdapter;
+
+    private ProgressBar mProgressCircle;
+
+    private DatabaseReference mDatabaseRef;
+    private List<Upload> mUploads;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mProgressCircle = findViewById(R.id.progress_circle);
+
+        mUploads = new ArrayList<>();
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    mUploads.add(upload);
+                }
+
+                mAdapter = new ImageAdapter(ImagesActivity.this, mUploads);
+
+                mRecyclerView.setAdapter(mAdapter);
+
+                mAdapter.setOnItemClickListener(ImagesActivity.this);
+
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ImagesActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(this, "Normal click at position: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onWhatEverClick(int position) {
+        Intent browserIntent = new
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://math.ru/lib/files/pdf/geometry/Ponarin-I.pdf"));
+        startActivity(browserIntent);
+
+    }
+
+    @Override
+    public void onDeleteClick(int position) {
+        Toast.makeText(this, "Delete click at position: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/firebase/Upload.java
@@ -0,0 +1,31 @@
+package com.example.firebase;
+
+public class Upload {
+    private String mName;
+    private String mImageUrl;
+
+    public Upload(){
+        //empty constructor needed
+
+    }
+    public Upload(String name, String imageUrl){
+        if(name.trim().equals("")){
+            name = "No name";
+        }
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+    public String getmName(){
+        return mName;
+    }
+    public void setmName(String name){
+        mName = name;
+    }
+    public String getmImageUrl(){
+        return mImageUrl;
+
+    }
+    public void setmImageUrl(String imageUrl){
+        mImageUrl = imageUrl;
+    }
+}
